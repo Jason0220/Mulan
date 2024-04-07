05c59208028b95e51d7c01db16d6b8e5ec924d45
jinx <Jason0220@users.noreply.github.com>
2024-04-07 17:41:47 +0800

    Update build_ota.sh

--- build_ota.sh
+++ build_ota.sh
@@ -4,9 +4,9 @@ if (( $# < 4 )) ; then
 	echo "params error"
 	echo "Usage: $0 NXP_DIR BES_DIR NXP_VER BES_VER"
 	echo "NXP_DIR: The parent dir of repository "Mulan-NXP595""
-	echo "BES_DIR: The parent dir of repository "Mulan-BES2500L""
+	
 	echo "NXP_VER: The NXP595 firmware version to be included in the OTA package"
-	echo "BES_VER: The BES2500 firmware version to be included in the OTA package"
+	
 	logger "params error"
 	logger "NXP_DIR: The parent dir of repository "Mulan-NXP595""
 	logger "BES_DIR: The parent dir of repository "Mulan-BES2500L""
@@ -49,18 +49,18 @@ function prepare_ota() {
 	# copy the necessary ota files to OTA_PATH
 	cd "$OTA_PATH"
 	echo -e "\ncd $(pwd)" | tee -a $LOG_FILE
-	cp $NXP_DIR/Mulan-NXP595/target/mulan/rtthread.bin . 2>&1 | tee -a $LOG_FILE
+	cp $NXP_DIR/Mulan-NXP595/target/mulan/rtthread.bin . >> $LOG_FILE 2>&1
 	echo "Copy NXP595 FW to $OTA_PATH result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
-	cp $BES_DIR/Mulan-BES2500L/out/best2500p_ibrt/best2500p_ibrt.bin.converted.bin ./bes.bin 2>&1 | tee -a $LOG_FILE
+	cp $BES_DIR/Mulan-BES2500L/out/best2500p_ibrt/best2500p_ibrt.bin.converted.bin ./bes.bin >> $LOG_FILE 2>&1
 	echo "Copy BES2500 OTA file to $OTA_PATH and rename bes.bin result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
 	
-	sed -i '36,55d' "$OTA_FILE" 2>&1 | tee -a $LOG_FILE
+	sed -i '36,55d' "$OTA_FILE" >> $LOG_FILE 2>&1
 	echo "Remove the lines 36~55 from $OTA_FILE result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
-	sed -i '16,25d' "$OTA_FILE" 2>&1 | tee -a $LOG_FILE
+	sed -i '16,25d' "$OTA_FILE" >> $LOG_FILE 2>&1
 	echo "Remove the lines 16~25 from $OTA_FILE result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
-	sed -i '12s/V4\.0/'"$NXP_VER"'/' "$OTA_FILE" 2>&1 | tee -a $LOG_FILE
+	sed -i '12s/V4\.0/'"$NXP_VER"'/' "$OTA_FILE" >> $LOG_FILE 2>&1
 	echo "Replace NXP new_firmware_version with $NXP_VER result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
-	sed -i '22s/V4\.0/'"$BES_VER"'/' "$OTA_FILE" 2>&1 | tee -a $LOG_FILE
+	sed -i '22s/V4\.0/'"$BES_VER"'/' "$OTA_FILE" >> $LOG_FILE 2>&1
 	echo "Replace BES new_firmware_version with $BES_VER result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
 	
 	return 0
@@ -69,12 +69,19 @@ function prepare_ota() {
 function ota_generation() {
 	cd $OTA_PATH
 	echo -e "\ncd $(pwd)" | tee -a $LOG_FILE
-	python ota_packager.py 2>&1 | tee -a $LOG_FILE
-	echo "Generate OTA upgrade bin file result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
-	for file in upgrade_2024-*.bin; do
-    	    mv "$file" "upgrade.bin"
-	done
-	echo "Rename OTA upgrade file as upgrade.bin result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
+	echo "Generating OTA upgrade file... result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
+	python ota_packager.py >> $LOG_FILE 2>&1
+	
+	# Find the last generated file starts with "upgrade" and get the file name
+    file=$(ls -t upgrade* | head -n 1)
+
+    # if the file exists, rename it as "upgrade.bin"
+    if [ -n "$file" ]; then
+        mv "$file" "upgrade.bin"
+        echo "Renamed $file to "upgrade.bin" result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
+    else
+        echo "OTA generation failed! result ${PIPESTATUS[0]}" | tee -a $LOG_FILE
+    fi
 	
 	return 0
 }
